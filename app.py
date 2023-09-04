d716c3427a07e31f3421c56f18c286db1f4b3940
Martin Castro <113154771+wmk3com@users.noreply.github.com>
2023-09-04 15:27:54 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle 
+
+cv = pickle.load(open("models/cv.pkl", "rb"))
+clf = pickle.load(open("models/clf.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST" :
+        email_text = request.form.get('email-content')
+    tokenized_email = cv.transform([email_text])
+    predictions = clf.predict(tokenized_email)
+    predictions = 1 if predictions == 1 else -1
+    return render_template("index.html", predictions = predictions, email_text=email_text)
+
+if __name__=="__main__":
+    app.run(debug=True)
